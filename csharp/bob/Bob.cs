bc6ad07239c7f5175ea8456251c0784e656da7ac
pete_jdv <redjoker011@gmail.com>
2022-09-09 15:29:26 +0800

    feat: solution for Bob

--- csharp/bob/Bob.cs
+++ csharp/bob/Bob.cs
@@ -1,9 +1,42 @@
 using System;
+using System.Text.RegularExpressions;
+using System.Linq;
 
 public static class Bob
 {
     public static string Response(string statement)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        string normalizedStatement = statement.Trim();
+
+        if (string.IsNullOrEmpty(normalizedStatement))
+        {
+            return "Fine. Be that way!";
+        }
+        else if (normalizedStatement.IsYell() && normalizedStatement.IsQuestion())
+        {
+            return "Calm down, I know what I'm doing!";
+        }
+        else if (normalizedStatement.IsQuestion())
+        {
+            return "Sure.";
+        }
+        else if (normalizedStatement.IsYell())
+        {
+            return "Whoa, chill out!";
+        }
+        else
+        {
+            return "Whatever.";
+        }
+    }
+
+    public static bool IsQuestion(this string message)
+    {
+        return message.EndsWith("?");
+    }
+
+    public static bool IsYell(this string message)
+    {
+        return message == message.ToUpper() && message.Any(char.IsLetter);
     }
 }
